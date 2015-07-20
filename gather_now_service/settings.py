91f679545ceae207f9727b7de3a902613d6c27ba
Sam Sheftel <mathias@mailinator.com>
2015-07-20 19:10:42 -0400

    remove people

--- gather_now_service/settings.py
+++ gather_now_service/settings.py
@@ -148,58 +148,7 @@ events = {
 }
 
 
-people = {
-  # 'title' tag used in item links.
-  'item_title': 'person',
-
-  # by default the standard item entry point is defined as
-  # '/people/<ObjectId>/'. We leave it untouched, and we also enable an
-  # additional read-only entry point. This way consumers can also perform GET
-  # requests at '/people/<lastname>/'.
-  'additional_lookup': {
-    'url': 'regex("[\w]+")',
-    'field': 'lastname'
-  },
-
-  # Schema definition, based on Cerberus grammar. Check the Cerberus project
-  # (https://github.com/nicolaiarocci/cerberus) for details.
-  'schema': {
-    'firstname': {
-      'type': 'string',
-      'minlength': 1,
-      'maxlength': 10,
-    },
-    'lastname': {
-      'type': 'string',
-      'minlength': 1,
-      'maxlength': 15,
-      'required': True,
-      # talk about hard constraints! For the purpose of the demo
-      # 'lastname' is an API entry-point, so we need it to be unique.
-      'unique': True,
-    },
-    # 'role' is a list, and can only contain values from 'allowed'.
-    'role': {
-      'type': 'list',
-      'allowed': ["author", "contributor", "copy"],
-    },
-    # An embedded 'strongly-typed' dictionary.
-    'location': {
-      'type': 'dict',
-      'schema': {
-        'address': {'type': 'string'},
-        'city': {'type': 'string'}
-      },
-    },
-    'born': {
-      'type': 'datetime',
-    },
-  }
-}
-
-
 DOMAIN = {
-  'people': people,
   'events': events
 }
 
