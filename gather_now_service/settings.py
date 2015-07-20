6e39cb2888df78d0be3016a3dc06bc3a2dd1315e
Sam Sheftel <mathias@mailinator.com>
2015-07-20 18:53:14 -0400

    remove workers

--- gather_now_service/settings.py
+++ gather_now_service/settings.py
@@ -147,38 +147,6 @@ events = {
   }
 }
 
-works = {
-  # if 'item_title' is not provided Eve will just strip the final
-  # 's' from resource name, and use it as the item_title.
-  #'item_title': 'work',
-
-  # We choose to override global cache-control directives for this resource.
-  'cache_control': 'max-age=10,must-revalidate',
-  'cache_expires': 10,
-
-  'schema': {
-    'title': {
-      'type': 'string',
-      'required': True,
-    },
-    'description': {
-      'type': 'string',
-    },
-    'owner': {
-      'type': 'objectid',
-      'required': True,
-      # referential integrity constraint: value must exist in the
-      # 'people' collection. Since we aren't declaring a 'field' key,
-      # will default to `people._id` (or, more precisely, to whatever
-      # ID_FIELD value is).
-      'data_relation': {
-        'resource': 'people',
-        # make the owner embeddable with ?embedded={"owner":1}
-        'embeddable': True
-      },
-    },
-  }
-}
 
 people = {
   # 'title' tag used in item links.
@@ -232,7 +200,6 @@ people = {
 
 DOMAIN = {
   'people': people,
-  'works': works,
   'events': events
 }
 
